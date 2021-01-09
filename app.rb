d0d58c45d792579b78d3763c534c331f811a3dc3
meggs1 <megan.schertz@gmail.com>
2021-01-09 15:35:42 -0600

    commit tree 16a5ae095a8aefb0fc679110214b3addcb33c5b2

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Megan Schertz"
+    end
+
+    get '/hometown' do
+        "My hometown is Saint Paul, Minnesota"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Love on the Brain"
+    end
 end
